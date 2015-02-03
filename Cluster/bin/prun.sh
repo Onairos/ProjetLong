639b253cc4188298bb5a428e35d70671b48d52f0
Aubeneau Simon <saubenea@serval.enseeiht.fr>
2015-02-03 17:32:00 +0100

    appel du bon mpirun

--- Cluster/bin/prun.sh
+++ Cluster/bin/prun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mpirun -np $1 --hostfile ../../hosts/hosts clusters $2 | tee output
+/mnt/hmf/mpich2/bin/mpirun -np $1 --hostfile ../../hosts/hosts clusters $2 | tee output
 ls cluster.final.* | wc -l > nbclusters
 
--- Cluster/bin/run.sh
+++ Cluster/bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mpirun -np $1 clusters $2 | tee output
+/mnt/hmf/mpich2/bin/mpirun -np $1 clusters $2 | tee output
 ls cluster.final.* | wc -l > nbclusters
 
