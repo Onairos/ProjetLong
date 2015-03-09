d48f5e66b8dc71751ca321ebb08f87314542807c
Soriano Tristan <tsoriano@kiss.enseeiht.fr>
2015-03-09 14:43:06 +0100

    diff method integration

--- Cluster/src/module_calcul.f90
+++ Cluster/src/module_calcul.f90
@@ -129,7 +129,349 @@ CONTAINS
     RETURN
   END SUBROUTINE get_sigma_interface
 
-  SUBROUTINE apply_spectral_clustering(numproc, nb_clusters_max, nbideal, partitioned_data, sigma)
+
+
+FUNCTION poly_kernel( dataw, gam, delta )
+    IMPLICIT NONE
+    !###########################################
+    ! DECLARATIONS
+    !###########################################
+    !#### Parameters ####
+    !====  IN  ====
+    TYPE(type_data) :: dataw
+    DOUBLE PRECISION ::gam 
+    DOUBLE PRECISION :: delta
+
+    !====  OUT  ====
+    DOUBLE PRECISION, DIMENSION(dataw%nb,dataw%nb) :: poly_kernel
+    !#### Variables  ####
+    DOUBLE PRECISION, DIMENSION(:,:), POINTER :: K
+    INTEGER :: n
+    INTEGER :: i
+    INTEGER :: j
+    INTEGER :: d
+
+    n=dataw%nb
+    ALLOCATE(K(n,n))
+    !ALLOCATE(polyKernel)
+    K(:,:)=0.0
+
+    DO i=1,n-1
+      DO j=1,n-1
+        DO d=1,dataw%dim
+        K(i,j)=K(i,j)+dataw%point(i)%coord(d)*dataw%point(j)%coord(d)
+        ENDDO 
+        K(i,j)=(K(i,j)+gam)**delta
+      ENDDO
+    ENDDO
+    poly_kernel=K
+    RETURN
+  END
+
+    FUNCTION gaussian_kernel( dataw, sigma )
+    !###########################################
+    ! DECLARATIONS
+    !###########################################
+    !#### Parameters ####
+    !====  IN  ====
+    TYPE(type_data) :: dataw
+    DOUBLE PRECISION sigma
+
+
+    !====  OUT  ====
+    DOUBLE PRECISION, DIMENSION(dataw%nb,dataw%nb) :: gaussian_kernel
+
+    !#### Variables  ####
+    DOUBLE PRECISION, DIMENSION(:,:), POINTER :: K
+    INTEGER n
+    INTEGER i
+    INTEGER j
+    INTEGER d
+
+    n=dataw%nb
+    ALLOCATE(K(n,n))
+    !ALLOCATE(gaussian_kernel)
+    K(:,:)=0.0
+
+    DO i=1,n-1
+      DO j=i+1,n
+        DO d=1,dataw%dim
+        K(i,j)=K(i,j)+(dataw%point(i)%coord(d)-dataw%point(j)%coord(d))**2
+        ENDDO
+        K(i,j)=exp(- K(i,j)/(2*sigma**2))
+        ! Symetry
+        K(j,i)=K(i,j)
+      ENDDO
+    ENDDO
+    gaussian_kernel=K
+    RETURN
+  END
+
+
+!Stop when converged compute E = sum_N(sum_M( Indicatrice (xi E Ck)*||phi(xi)-mk||²))
+
+
+
+SUBROUTINE apply_kernel_k_means(numproc,nblimit,nbideal,dataw,ker_param)
+    IMPLICIT NONE
+
+  ! INCLUDE 'mpif.h'
+    !###########################################
+    ! DECLARATIONS
+    !###########################################
+    !#### Parameters ####
+    !====  IN  ====
+  
+    INTEGER :: nbideal
+    INTEGER :: nblimit
+    INTEGER :: numproc
+    TYPE(type_kernel) :: ker_param
+
+    !=== IN/OUT ===
+    TYPE(type_data) :: dataw
+
+
+    !###########################################
+    ! DECLARATIONS
+    !###########################################      
+    !#### Variables  ####
+
+    DOUBLE PRECISION, DIMENSION(:,:), POINTER :: Ker
+    INTEGER :: it_max ! the maximum number of iterations
+    DOUBLE PRECISION :: cluster_center (dataw%dim, dataw%nbclusters) ! the cluster centers
+    DOUBLE PRECISION :: cluster_energy (dataw%nbclusters) ! the cluster energies
+    INTEGER :: it_num ! the number of iterations taken
+    INTEGER :: cluster (dataw%nb) ! indicates which cluster each point belongs to
+    INTEGER :: cluster_population (dataw%nbclusters) ! the number of points in each cluster
+    DOUBLE PRECISION :: listnorm (dataw%nb, dataw%nbclusters)
+    DOUBLE PRECISION :: stockcenter (dataw%dim, dataw%nbclusters)
+    DOUBLE PRECISION :: stockenergy (dataw%nbclusters)
+    DOUBLE PRECISION :: norme
+    DOUBLE PRECISION :: seuil
+    DOUBLE PRECISION :: val
+    DOUBLE PRECISION :: valmax
+    INTEGER :: cluster_id (dataw%nbclusters)
+    INTEGER :: stockpopulation (dataw%nbclusters)
+    INTEGER :: i
+    INTEGER :: j
+    INTEGER :: k
+    INTEGER :: l
+
+   ! LOGICAL :: found
+    DOUBLE PRECISION :: num1
+    DOUBLE PRECISION :: den1
+    DOUBLE PRECISION :: num2
+    DOUBLE PRECISION :: den2
+
+    INTEGER :: ok ! TODO: booleen ?
+    INTEGER :: ok2 ! TODO: booleen ?
+    INTEGER :: swap
+    INTEGER :: p
+    
+
+    ALLOCATE(Ker(dataw%nb,dataw%nb))
+    Ker(:,:)=0.0
+
+
+    !###########################################      
+    ! INSTRUCTIONS
+    !###########################################   
+    it_num = 0
+    !
+    !  Idiot checks.
+    !
+    IF ( dataw%nbclusters < 1 ) THEN
+       WRITE ( *, '(a)' ) ' '
+       WRITE ( *, '(a)' ) 'KERNELKMEANS_01 - Fatal error!'
+       WRITE ( *, '(a)' ) '  CLUSTER_NUM < 1.0'
+       STOP
+    ENDIF
+
+    IF ( dataw%dim < 1 ) THEN
+       WRITE ( *, '(a)' ) ' '
+       WRITE ( *, '(a)' ) 'KERNELKMEANS_01 - Fatal error!'
+       WRITE ( *, '(a)' ) '  DIM_NUM < 1.0'
+       STOP
+    ENDIF
+
+    IF ( dataw%nb < 1 ) THEN
+       WRITE ( *, '(a)' ) ' '
+       WRITE ( *, '(a)' ) 'KERNELKMEANS_01 - Fatal error!'
+       WRITE ( *, '(a)' ) '  POINT_NUM < 1.0'
+       STOP
+    ENDIF
+
+ 
+    IF (ker_param%kernelfunindex==0 .AND. (ker_param%gam <0.0 .OR. ker_param%delta<0.0) )  THEN
+       WRITE ( *, '(a)' ) ' '
+       WRITE ( *, '(a)' ) 'KERNELKMEANS_01 - Fatal error!'
+       WRITE ( *, '(a)' ) '  GAMMA AND DELTA NOT INITIALIZED IN POLYNOMIAL KERNEL'
+       STOP  
+    
+
+    ELSEIF (ker_param%kernelfunindex==1 .AND. ker_param%sigma<0.0) THEN
+       WRITE ( *, '(a)' ) ' '
+       WRITE ( *, '(a)' ) 'KERNELKMEANS_01 - Fatal error!'
+       WRITE ( *, '(a)' ) '  SIGMA NOT INITIALIZED IN GAUSSIAN KERNEL'
+       STOP 
+    ENDIF
+
+
+    IF (ker_param%kernelfunindex==0) THEN
+        Ker=poly_kernel( dataw, ker_param%gam, ker_param%delta)
+    ELSEIF (ker_param%kernelfunindex==1) THEN
+        Ker=gaussian_kernel(dataw, ker_param%sigma)
+    ENDIF
+
+
+
+    !
+    !  For each observation, calculate the distance from each cluster
+    !  center, and assign to the nearest.
+    !
+
+    !
+    !  Assign one point to each cluster center.
+    !
+    cluster_center(:,1) = dataw%point(1)%coord(:) !point(:,1) %%
+    cluster_id(:)=0; cluster_id(1)=1
+    p=2
+    seuil=0.4
+!#if aff
+PRINT *, 'recherche des centres'
+!#endif
+    DO i = 2, dataw%nbclusters
+       ok=0
+       DO WHILE(ok==0)
+          valmax=2.0*seuil
+          !recherche si le point est deja utilise dans comme centre
+          ok2=0
+          DO j=1,i-1
+             IF (dataw%point(j)%cluster==p) ok2=1
+          ENDDO
+          !si point pas centre, teste par rapport au seuil
+          IF (ok2==0) THEN
+             DO j=1,i-1
+                val=0.0; norme=0.0
+                DO k=1,dataw%dim
+                   val=max(val,abs(cluster_center(k,j)-dataw%point(p)%coord(k))) 
+!VOIR SI CELA DOIT ÊTRE MODIFIE EN FONCTION DES KERNEL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                ENDDO
+                valmax=min(val,valmax)
+             ENDDO
+             IF (valmax>=seuil) ok=1
+          ENDIF
+         p=p+1
+
+         !abaisse le seuil si pas assez de centre sont trouves
+         IF ((p>dataw%nb).AND.(ok==0)) THEN 
+            seuil=0.9*seuil
+!#if aff
+            PRINT *,'abaisse seuil :',seuil
+!#endif
+            p=1
+          ENDIF
+       ENDDO
+       p=p-1
+       cluster_center(:,i)= dataw%point(P)%coord(:) !point(:,p) 
+       cluster_id(i)=p
+    ENDDO
+!#if aff
+   PRINT *,'centres initiaux',p
+!#endif
+!cluster_center
+
+!!! boucle            
+    it_num = 0
+    swap=1
+    dataw%point(:)%cluster=1 !  cluster(:)=1
+    DO WHILE ((it_num<it_max).AND.(swap/=0))
+       it_num = it_num + 1
+       swap=0
+       DO i=1,dataw%nbclusters
+          stockenergy(i)=cluster_energy(i);
+          stockpopulation(i)=cluster_population(i);
+          DO j=1,dataw%dim
+             stockcenter(j,i)=cluster_center(j,i)
+          ENDDO
+       ENDDO
+
+       !! Calcul de toutes les distances
+       cluster_population(1:dataw%nbclusters) = 1
+       listnorm(:,:)=0.0
+       num1=0.0
+       den1=0.0
+       num2=0.0
+       den2=0.0
+       DO k=1,dataw%nbclusters
+           DO i=1,dataw%nb
+               DO j=1,dataw%nb
+                   IF ( dataw%point(j)%cluster.EQ.k) THEN
+                   num1=num1 + 2*(Ker(i,j))
+                   den1=den1+1 
+                   ENDIF
+                   DO l=1,dataw%nb
+                       IF ( dataw%point(j)%cluster.EQ.k .AND. dataw%point(l)%cluster.EQ.k) THEN
+                       num2=num2 + Ker(j,l)
+                       den2=den2+1
+                       ENDIF
+                   ENDDO
+               ENDDO
+           listnorm(i,k)= Ker(i,i)
+               IF (den1.NE.0.0) THEN
+               listnorm(i,k)=  listnorm(i,k) - (num1/den1)
+               ENDIF
+               IF (den2.NE.0.0) THEN
+               listnorm(i,k)=  listnorm(i,k)  + (num2/den2)
+               ENDIF
+           ENDDO
+       ENDDO
+     
+
+       !!assignation par rapport au min des distances
+       cluster_population(:)=0
+       DO i=1,dataw%nb
+          DO j=1,dataw%nbclusters
+             IF (listnorm(i,j)<listnorm(i,dataw%point(i)%cluster)) THEN
+                dataw%point(i)%cluster=j
+                swap=swap+1
+             ENDIF
+          ENDDO
+          cluster_energy(dataw%point(i)%cluster)=cluster_energy(dataw%point(i)%cluster)&
+               +listnorm(i,dataw%point(i)%cluster)
+          cluster_population(dataw%point(i)%cluster)=cluster_population(dataw%point(i)%cluster)+1
+       ENDDO
+
+       !! mise a jour des centres
+       cluster_center(:,:)=0.0
+       DO j=1,dataw%nb
+          i=dataw%point(j)%cluster 
+          DO k=1,dataw%dim
+             cluster_center(k,i)=cluster_center(k,i)+dataw%point(j)%coord(k)
+          ENDDO
+       ENDDO
+       DO i=1,dataw%nbclusters
+          cluster_center(:,i)=cluster_center(:,i)/cluster_population(i)
+       ENDDO
+
+
+
+    ENDDO
+
+    RETURN
+  END SUBROUTINE apply_kernel_k_means
+
+
+
+
+
+
+
+
+
+
+
+  SUBROUTINE apply_spectral_clustering(numproc, nb_clusters_max, nbideal, partitioned_data, sigma,ker_param)
     IMPLICIT NONE
     INCLUDE 'mpif.h'
     !###########################################
@@ -141,6 +483,7 @@ CONTAINS
     INTEGER :: nbideal
     INTEGER :: nb_clusters_max
     INTEGER :: numproc
+    TYPE(type_kernel) :: ker_param
 
     !=== IN/OUT ===
     TYPE(type_data) :: partitioned_data
@@ -404,5 +747,163 @@ PRINT *, 'DEBUG : Frobenius ratio'
 
     RETURN
   END SUBROUTINE apply_spectral_clustering
+
+
+
+SUBROUTINE mean_shift(numproc,nblimit,nbideal,dataw,bandWidth)
+
+   !INCLUDE 'mpif.h'
+    !IMPLICIT NONE
+    !###########################################
+    ! DECLARATIONS
+    !###########################################
+    !#### Parameters ####
+    !====  IN  ====
+  
+    INTEGER :: nbideal
+    INTEGER :: nblimit
+    INTEGER :: numproc
+    DOUBLE PRECISION :: bandWidth !bandwidth parameter
+
+    !=== IN/OUT ===
+    TYPE(type_data) :: dataw
+    
+    !###########################################
+    ! DECLARATIONS
+    !###########################################      
+    INTEGER ::point_num												!number of points
+    INTEGER ::dim_num													!number of dimensions
+    INTEGER ::cluster_num												!number of clusters
+    
+    !#### Variables  ####
+    INTEGER :: numClust 												!the cluster number  
+    DOUBLE PRECISION :: bandSq										!square of bandWidth
+    DOUBLE PRECISION :: stopThresh									!when mean has converged
+    INTEGER :: beenVisitedFlag(dataw%nb)								!track if a point has been seen already
+    INTEGER :: numInitPts												!number of points to possibly use as initialization points
+    INTEGER :: thisClusterVotes(dataw%nb)								!used to resolve conflicts on cluster membership
+    INTEGER :: stInd													!start point of mean
+    DOUBLE PRECISION :: myMean(dataw%dim)								!mean of this cluster
+    DOUBLE PRECISION :: myOldMean(dataw%dim)							!old mean computed for this cluster
+    INTEGER :: myMembers(dataw%nb)										!1 if the point belongs to the cluster, else 0
+    INTEGER :: mergeWith												!used to merge clusters
+    DOUBLE PRECISION :: clustCent(dataw%dim,dataw%nbclusters)			!centers of each cluster
+	INTEGER :: clusterVotes(dataw%nbclusters,dataw%nb)					!number of votes for each point for each cluster
+    INTEGER :: i
+    INTEGER :: j
+    INTEGER :: num
+    DOUBLE PRECISION :: sqDist
+    
+    
+    
+    !INITIALIZE STUFF    
+    point_num=dataw%nb
+    dim_num=dataw%dim
+    numClust = 1
+    bandSq = bandWidth**2
+    stopThresh = 1e-3*bandWidth
+    beenVisitedFlag(:) = 0
+    numInitPts = point_num
+    clusterVotes(:,:) = 0		
+    
+    DO WHILE (numInitPts>0)
+    
+		!take the first point as start of mean
+		DO i=1, point_num
+			IF (beenVisitedFlag(i)==0) THEN
+				stInd = i
+				EXIT
+			ENDIF
+		ENDDO
+		myMean = dataw%point(stInd)%coord	!initialize mean to this points location
+		DO j=1, dim_num
+			myMean(j) = dataw%point(i)%coord(j)
+		ENDDO
+		myMembers(:) = 0
+		thisClusterVotes(:) = 0	!used to resolve conflicts on cluster membership
+		
+		DO
+		
+			DO i=1, point_num
+				!dist squared from mean to all points still active
+				sqDist = 0
+				DO j=1, dim_num
+					sqDist = sqDist + (dataw%point(i)%coord(j) - myMean(j))**2
+				ENDDO
+				IF (sqDist < bandSq) THEN
+					thisClusterVotes(i) = thisClusterVotes(i) + 1	!add a vote for all the in points belonging to this cluster
+					myMembers(i) = 1								!add any point within bandWidth to the cluster
+					beenVisitedFlag(i) = 1							!mark that these points have been visited
+				ENDIF
+			ENDDO
+			
+			myOldMean = myMean
+			
+			!compute the new mean
+			DO i=1, point_num
+				num = 0
+				IF (myMembers(i)==1) THEN
+					DO j=1, dim_num
+						myMean(j) = myMean(j) + dataw%point(i)%coord(j)
+					ENDDO
+					num = num + 1
+				ENDIF
+			ENDDO
+			myMean = myMean/num
+			
+			!compute the distance from myMean to myOldMean
+			sqDist = 0
+			DO j=1, dim_num
+				sqDist = sqDist + (myOldMean(j) - myMean(j))**2
+			ENDDO
+			
+			!if mean doesn't move much stop this cluster
+			IF (sqDist < stopThresh**2) THEN
+			
+				!check for merge posibilities
+				mergeWith = 0
+				DO cN=1, numclust-1
+					!compute the distance from possible new clust max to old clust max
+					sqDist = 0
+					DO j=1, dim_num
+						sqDist = sqDist + (clustCent(j,cN) - myMean(j))**2
+					ENDDO
+					IF (sqDist < (bandWidth/2)**2) THEN
+						mergeWith = cN
+						EXIT
+					ENDIF
+				ENDDO
+				
+				IF (mergeWith > 0) THEN		!something to merge
+				
+					clustCent(:,mergeWith) = (myMean+clustCent(:,mergeWith))/2					!mean of centers
+					clusterVotes(mergeWith,:) = clusterVotes(mergeWith,:) + thisClusterVotes;	!add these votes to the merged cluster
+					
+				ELSE
+				
+					numClust = numClust + 1
+					clustCent(:,numClust) = myMean
+					clusterVotes(numClust,:) = thisClusterVotes
+				ENDIF
+				EXIT
+				
+			ENDIF
+		
+		ENDDO
+			
+		DO i=1, point_num
+			numInitPts = 0
+			IF (beenVisitedFlag(i)==0) THEN
+				numInitPts = numInitPts + 1
+			ENDIF
+		ENDDO
+			
+	ENDDO
+	
+	DO i=1, point_num
+		dataw%point(i)%cluster = MAXLOC(clusterVotes(:,i), DIM=1)
+	ENDDO
+	
+END SUBROUTINE mean_shift
 
 END MODULE module_calcul
